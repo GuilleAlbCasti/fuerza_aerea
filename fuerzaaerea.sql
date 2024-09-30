2f6d984472daea48f4c53239e7ebe45af17d43a0
Nicolás <nicolas_mateos@yahoo.com>
2024-09-30 20:25:59 -0300

    Add files via upload

---
+++ fuerzaaerea.sql
@@ -0,0 +1,98 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 01-10-2024 a las 01:20:44
+-- Versión del servidor: 10.4.32-MariaDB
+-- Versión de PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `fuerzaaerea`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `avion`
+--
+
+CREATE TABLE `avion` (
+  `id` int(11) NOT NULL,
+  `modelo` varchar(20) NOT NULL,
+  `anio` int(11) NOT NULL,
+  `origen` varchar(20) NOT NULL,
+  `horas_vuelo` int(11) NOT NULL,
+  `base_fk` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `base`
+--
+
+CREATE TABLE `base` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(40) NOT NULL,
+  `ubicacion` varchar(20) NOT NULL,
+  `capacidad` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `avion`
+--
+ALTER TABLE `avion`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `base_fk` (`base_fk`);
+
+--
+-- Indices de la tabla `base`
+--
+ALTER TABLE `base`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `avion`
+--
+ALTER TABLE `avion`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `base`
+--
+ALTER TABLE `base`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `avion`
+--
+ALTER TABLE `avion`
+  ADD CONSTRAINT `avion_ibfk_1` FOREIGN KEY (`base_fk`) REFERENCES `base` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
