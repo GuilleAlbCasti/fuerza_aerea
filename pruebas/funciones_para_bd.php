ed20adedb731cc36137ad37b82af9681fbc37a2f
GuilleAlbCasti <guille_casti@hotmail.com>
2024-10-04 11:55:31 -0300

    Preparando consultas a la bases de datos

---
+++ pruebas/funciones_para_bd.php
@@ -0,0 +1,48 @@
+<?php
+
+// CONEXIÓN CON BD
+
+function conexion_bd() {
+    return new PDO('mysql:host=localhost;dbname=fuerzaaerea;charset=utf8', 'root', '');
+}
+
+// OBTENER LISTA DE BASES AÉREAS
+
+function traer_bases() {
+    $db = conexion_bd();
+    $query = $db->prepare('SELECT * FROM base ORDER BY id ASC');
+    $query->execute();
+
+    return $query->fetchAll(PDO::FETCH_OBJ);
+}
+
+// SELECIONAR 1 BASE AÉREA
+
+function traer_base($id_base) {
+    $db = conexion_bd();
+    $query = $db->prepare('SELECT * FROM base WHERE id_base = ?');
+    $query->execute(array($id_base));
+
+    return $query->fetch(PDO::FETCH_OBJ);
+}
+
+// OBTENER LISTA DE AVIONES
+
+function traer_aviones() {
+    $db = conexion_bd();
+    $query = $db->prepare('SELECT * FROM avion ORDER BY modelo ASC');
+    $query->execute();
+
+    return $query->fetchAll(PDO::FETCH_OBJ);
+}
+
+// SELECIONAR 1 AVION
+
+function traer_avion($id_avion) {
+    $db = conexion_bd();
+    $query = $db->prepare('SELECT * FROM avion WHERE id_avion = ?');
+    $query->execute(array($id_avion));
+
+    return $query->fetch(PDO::FETCH_OBJ);
+}
+
